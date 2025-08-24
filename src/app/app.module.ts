d889b2ad39cfb133d332aa9c5ca56f9511bce82a
Vicente Espinoza <vicente.espinoza@amaris.com>
2025-08-25 00:40:22 +0200

    Added i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {PatientModalComponent} from './components/patient-modal/patient-modal.component';
 import {PatientListComponent} from './components/patient-list/patient-list.component';
+import {TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   imports: [
@@ -16,7 +22,15 @@ import {PatientListComponent} from './components/patient-list/patient-list.compo
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    NgbModule
+    NgbModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   declarations: [AppComponent, PatientListComponent, PatientModalComponent],
   bootstrap: [AppComponent]
--- src/app/components/patient-modal/patient-modal.component.ts
+++ src/app/components/patient-modal/patient-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, EventEmitter, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import {EMPTY, Subject, throwError } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import {EMPTY, Subject } from 'rxjs';
 import {catchError, finalize, takeUntil, tap } from 'rxjs/operators';
 import { Patient, PatientUpdateRequest } from '../../models/patient';
 import { PatientService } from '../../services/patient.service';
@@ -30,7 +31,8 @@ export class PatientModalComponent implements OnInit, OnDestroy {
 
   constructor(
     private formBuilder: FormBuilder,
-    private patientService: PatientService
+    private patientService: PatientService,
+    private translate: TranslateService
   ) {
   }
 
@@ -126,26 +128,29 @@ export class PatientModalComponent implements OnInit, OnDestroy {
   getFieldError(fieldName: string): string {
     const control = this.patientForm.get(fieldName);
     if (control && control.errors && control.touched) {
+      const fieldDisplayName = this.getFieldDisplayName(fieldName);
+
       if (control.errors['required']) {
-        return `${this.getFieldDisplayName(fieldName)} is required`;
+        return this.translate.instant('validation.required', { field: fieldDisplayName });
       }
       if (control.errors['minlength']) {
-        return `${this.getFieldDisplayName(fieldName)} must be at least ${control.errors['minlength'].requiredLength} characters`;
+        return this.translate.instant('validation.minlength', {
+          field: fieldDisplayName,
+          requiredLength: control.errors['minlength'].requiredLength
+        });
       }
       if (control.errors['maxlength']) {
-        return `${this.getFieldDisplayName(fieldName)} must be at most ${control.errors['maxlength'].requiredLength} characters`;
+        return this.translate.instant('validation.maxlength', {
+          field: fieldDisplayName,
+          requiredLength: control.errors['maxlength'].requiredLength
+        });
       }
     }
     return '';
   }
 
   private getFieldDisplayName(fieldName: string): string {
-    const displayNames: { [key: string]: string } = {
-      familyName: 'Family Name',
-      givenName: 'Given Name',
-      sex: 'Sex'
-    };
-    return displayNames[fieldName] || fieldName;
+    return this.translate.instant(`validation.${fieldName}`);
   }
 
   hasFieldError(fieldName: string): boolean {
