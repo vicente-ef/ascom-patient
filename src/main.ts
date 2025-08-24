77a1078c243e72a80b0f6a1b9a7f9a91d889d633
Vicente Espinoza <vicente.espinoza@amaris.com>
2025-08-24 22:14:23 +0200

    Updates application loading page

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import './polyfills';
-
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+
+declare global {
+  interface Window {
+    ngRef: any;
+  }
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
@@ -11,6 +14,4 @@ platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
     window['ngRef'].destroy();
   }
   window['ngRef'] = ref;
-
-  // Otherwise, log the boot error
 }).catch(err => console.error(err));
